b49845300ebad36fd84580f3cdcc894d92588ed5
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-17 12:41:31 +0530

    Create Bank_Manager.java

---
+++ Encapsulation/Bank_Manager.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+class Bank_Manager
+{
+    public static void main(String[] args)
+    {
+
+        Client_details client1= new Client_details();
+
+        client1.setAcc_no(23695971);
+        client1.setName("Ponni");
+        client1.setAmount(500000);
+
+        System.out.println("Account No: "+client1.getAcc_no());
+        System.out.println("Account Name: "+client1.getName());
+        System.out.println("Account Balance: "+client1.getAmount());
+    }
+
+
+}
