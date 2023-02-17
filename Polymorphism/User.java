001511d378c6c299c8ce4609bdb356f4ee8a01d8
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-17 20:08:21 +0530

    Update User.java

--- Polymorphism/User.java
+++ Polymorphism/User.java
@@ -1,44 +1,31 @@
-package Polymorphism;
 
-class Stationery {
-    void total(String str) {
-        if(str=="Pen")
-        System.out.println("Cost of pen is 10");
-        else if(str=="Pencil")
-            System.out.println("Cost of pencil is 5");
-        else
-            System.out.println("Cost of eraser is 4");
-
-    }
-
-    void total(String str1,String str2)
+class Calculator 
+{
+    public int subtract(int a, int b) 
     {
-        if(str1=="Pen"||str1=="Pencil" && str2=="Pencil"||str2=="Pen")
-        System.out.println("Cost of pen and pencil is 15");
-        else if(str1=="Eraser"||str1=="Pencil" && str2=="Pencil"||str2=="Eraser")
-            System.out.println("Cost of pencil and Eraser is 9");
-        else
-            System.out.println("Cost of pen and Eraser is 14");
+        return a - b;
+    }
 
-    }
-    void total(String str1,String str2,String str3)
+    public double subtract(double a, double b) 
     {
-        System.out.println("Cost of pen,pencil and eraser is 19");
+        return a - b;
     }
+
+    public int subtract(int a, int b, int c) 
+    {
+        return a - b - c;
+    }
+    
 }
 
+public class OverLoadingExample
+{
+    public static void main(String[] args) 
+    {
+        Calculator calculator = new Calculator();
 
-public class User {
-    public static void main(String[] args) {
-        Stationery user1 = new Stationery();
-        user1.total("Pen");
-        user1.total("Pencil");
-        user1.total("Eraser");
-        user1.total("Pen"," Pencil");
-        user1.total("Pencil","Eraser");
-
-        user1.total("Pen","Pencil","Eraser");
-
-
+        System.out.println(calculator.subtract(2, 1));
+        System.out.println(calculator.subtract(5.5, 3.7));
+        System.out.println(calculator.subtract(9, 3, 1));
     }
 }
