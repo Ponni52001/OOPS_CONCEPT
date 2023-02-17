860cc21a8055647fb0a630461f86ac5fdea8ba76
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-17 19:41:01 +0530

    Update Bank_Manager.java

--- Encapsulation/Bank_Manager.java
+++ Encapsulation/Bank_Manager.java
@@ -5,15 +5,16 @@ class BankManager
     public static void main(String[] args)
     {
 
-        Client_details client1= new Client_details();
+        ClientDetails client= new ClientDetails();
 
-        client1.setAcc_no(23695971);
-        client1.setName("Ponni");
-        client1.setAmount(500000);
+        client.setAccountNumber(852364971);
+        client.setAccountName("Ponni");
+        client.setAmount(500000);
 
-        System.out.println("Account No: "+client1.getAcc_no());
-        System.out.println("Account Name: "+client1.getName());
-        System.out.println("Account Balance: "+client1.getAmount());
+
+        System.out.println("Account No: "+client.getAccountNumber());
+        System.out.println("Account Name: "+client.getAccountName());
+        System.out.println("Account Balance: "+client.getAmount());
     }
 
 
