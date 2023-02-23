72802329639f82f12627831ace7baf96f6e113cc
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-23 19:12:18 +0530

    Update SingleDigitNumber.java

--- LogicalProgramming/SingleDigitNumber.java
+++ LogicalProgramming/SingleDigitNumber.java
@@ -1,13 +1,14 @@
 package SteppingNumber;
 
-public class SingleDigitNumber 
-{
-    public void isSingleDigit(int startingNumber) 
-    {
-        while (startingNumber <= 9 & startingNumber >= 0) 
-        {
+public class SingleDigitNumber {
+    int number;
+    public void isSingleDigit(int startingNumber) {
+        while (startingNumber <= 9 & startingNumber >= 0) {
             System.out.println(startingNumber);
             startingNumber++;
+
         }
+        number=startingNumber;
     }
+
 }
