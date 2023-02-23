b1ff3aa6e041420ee6987d2307775152d5c7d2e6
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-23 18:20:47 +0530

    Create Main.java

---
+++ LogicalProgramming/Main.java
@@ -0,0 +1,29 @@
+package SteppingNumber;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Starting number:");
+        int startingNumber=sc.nextInt();
+        System.out.println("Enter the Ending number:");
+        int endingNumber = sc.nextInt();
+
+        if(startingNumber>endingNumber)
+        {
+            System.out.println("Sorry you have entered a wrong range!!!");
+        }
+
+        SingleDigitNumber task1 = new SingleDigitNumber();
+        task1.isSingleDigit(startingNumber);
+
+        SteppingNumbers numbers = new SteppingNumbers();
+        numbers.printSteppingNumber(startingNumber,endingNumber);
+
+
+
+    }
+}
