2f1427e637e42823f41d9206cfe4d44724d130ec
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-02-24 12:11:21 +0530

    Create LargestSubarray.java

---
+++ LargestSubarray/LargestSubarray.java
@@ -0,0 +1,43 @@
+package EqualZeroOne;
+
+public class LargestSubArray {
+    public void findLargestSubarray(int array[],int lengthOfArray)
+    {
+        int zeros=0;
+        int ones=0;
+        for(int i=0;i< array.length;i++)
+        {
+            if (array[i] == 0)
+            {
+                zeros++;
+            }
+            else {
+                ones++;
+            }
+        }
+
+        System.out.println(zeros == ones ? "The Index of the Subarray starts from "+"0 to " + (array.length - 1) : (zeros == 0 || ones == 0 ? "No sub array found" : ""));
+
+        for(int i=0;i<array.length;i++)
+        {
+            if(zeros>ones&&ones!=0)
+            {
+                if(array[i]==0)
+                {
+                    System.out.println("The Index of the Subarray starts from "+(i+1)+" to "+(array.length-1));
+                    break;
+                }
+            }
+            else if(ones>zeros&&zeros!=0)
+            {
+                if(array[i]==1)
+                {
+                    System.out.println("The Index of the Subarray starts from "+(i+1)+" to "+(array.length-1));
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
