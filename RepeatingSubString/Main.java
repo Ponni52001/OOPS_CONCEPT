8ed5d9ac0b0dad8d91a883f242348fd54baed935
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-01 18:26:41 +0530

    Update Main.java

--- RepeatingSubString/Main.java
+++ RepeatingSubString/Main.java
@@ -2,16 +2,19 @@ package SubString;
 
 import java.util.Scanner;
 
-class Main2 {
+class Main2
+{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sentence:");
         String sentence = sc.nextLine().toLowerCase();
+
         String[] array = sentence.split(" ");
         RepeatingSubstring subString = new RepeatingSubstring();
         subString.findRepeatingSubstring(array);
 
         }
-    }
+}
+
 
