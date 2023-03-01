4ffbf7025f48eb7528b1ef8500c370e72de0730a
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-01 18:27:11 +0530

    Update RepeatingSubstring.java

--- RepeatingSubString/RepeatingSubstring.java
+++ RepeatingSubString/RepeatingSubstring.java
@@ -5,12 +5,13 @@ public class RepeatingSubstring {
         for (int i = 0; i < array.length - 1; i++) {
             char[] word1 = array[i].toCharArray();
             for (int j = i + 1; j < array.length; j++) {
+
                 char[] word2 = array[j].toCharArray();
                 int count = 0;
                 int start = 0;
                 boolean output = false;
                 int z = 0;
-                for (int k = 0; k < word2.length & z < word1.length; k++) {
+                for (int k = 0; k < word2.length && z < word1.length; k++) {
                     if (word2[k] == word1[z]) {
                         if (!output) {
                             start = z;
@@ -20,23 +21,54 @@ public class RepeatingSubstring {
                         count++;
                     } else {
                         z++;
-                        k--;
+                        k = -1;
+                        output = false;
+                        count = 0;
                     }
                 }
-
                 if (count >= 2) {
-
                     while (count > 0) {
+
                         System.out.print(word1[start]);
                         start++;
                         count--;
                     }
 
-                    System.out.print(" ");
+                    System.out.println();
                 }
-
             }
+        }
+
+        for (String s : array) {
+            char[] word1 = s.toCharArray();
+            int z = 0;
+            int count = 0;
+            boolean first = false;
+            int start = 0;
+            for (int j = 1; j < word1.length; j++) {
+                if (word1[z] == word1[j]) {
+                    if (!first) {
+                        start = z;
+                        count++;
+                        first = true;
+                        z++;
+                    } else {
+                        count++;
+                        z++;
+                    }
+                }
+            }
+            if (count > 2) {
+                while (count > 0) {
+                    System.out.print(word1[start]);
+                    start++;
+                    count--;
+                }
+                System.out.println();
+            }
+
         }
 
     }
 }
+
