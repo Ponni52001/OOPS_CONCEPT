f96d092a5803db20a3ce927a4a0e0a9f5bb46bbf
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-02 21:52:53 +0530

    Create GasolineEngine.java

---
+++ SOLID_PRINCIPLES/Liskov_Principle/GasolineEngine.java
@@ -0,0 +1,18 @@
+package com.Liskov;
+
+
+class GasolineEngine implements Engine
+
+{
+
+    public void start() 
+    {
+        System.out.println("Starting the Gasoline Engine");
+    }
+
+    public void stop() 
+    {
+        System.out.println("Stoping the Gasoline Engine");
+    }
+    
+}
