b9befc37f61effd5b0595c2df1d34d9b675b6b69
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-02 21:49:56 +0530

    Create Main.java

---
+++ SOLID_PRINCIPLES/Liskov_Principle/Main.java
@@ -0,0 +1,16 @@
+package com.Liskov1;
+
+public class Main
+{
+  
+    public static void main(String[] args)
+    
+    {
+        ElectricEngine electricEngine = new ElectricEngine();
+      
+        electricEngine.start();
+        electricEngine.stop();
+      
+        //electricEngine.fillfuel();
+    }
+}
