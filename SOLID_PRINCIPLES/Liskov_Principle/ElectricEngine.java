03054aeda43e76d919beff860f2adf37ab90683a
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-02 21:50:27 +0530

    Create ElectricEngine.java

---
+++ SOLID_PRINCIPLES/Liskov_Principle/ElectricEngine.java
@@ -0,0 +1,15 @@
+package com.Liskov1;
+
+class ElectricEngine implements Engine 
+
+{
+    public void start() 
+    {
+        System.out.println("Starting the electric motor");
+    }
+
+    public void stop() 
+    {
+        System.out.println("Stopping the electric motor");
+    }
+}
