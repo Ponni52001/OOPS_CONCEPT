33b5c9f41cce190ec4e9bdf365311056cce8012a
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-03 20:42:40 +0530

    Create PrintLargestSubArray.java

---
+++ Logical_Programming/LargestSubarray/PrintLargestSubArray.java
@@ -0,0 +1,32 @@
+package NewCount;
+
+
+import java.util.Scanner;
+
+ class PrintLargestSubArray 
+ {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int sizeOfArray = sc.nextInt();
+        int array[] = new int[sizeOfArray];
+        int zero = 0;
+        int one = 0;
+        System.out.println("Enter the values:");
+        for (int i = 0; i < sizeOfArray; i++) {
+            array[i] = sc.nextInt();
+            if (array[i] == 0)
+            {
+                zero++;
+            }
+            else
+            {
+                one++;
+            }
+        }
+        LargestSubarray subArray=new LargestSubarray();
+        subArray.printLargestSubarray(array,zero,one,sizeOfArray);
+
+    }
+}
