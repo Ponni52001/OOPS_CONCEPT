39ef0cad48cf2f9002775a2899b0330d8e580fe0
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-06 11:55:53 +0530

    Update Worker.java

--- SOLID_PRINCIPLES/Interface_Segregation_Principle/Worker.java
+++ SOLID_PRINCIPLES/Interface_Segregation_Principle/Worker.java
@@ -3,7 +3,8 @@ package com.ISP;
 public class Worker implements PlaceOrder
 {
 
-    public void placeOrder() {
+    public void placeOrder()
+    {
         System.out.println("PLacing the customer order");
     }
 }
