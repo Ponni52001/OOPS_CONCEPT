f41883e263e973bcf33a7b4a3b7bf3a7836aafa2
Ponni52001 <103437520+Ponni52001@users.noreply.github.com>
2023-03-06 11:55:19 +0530

    Create PlaceOrder.java

---
+++ SOLID_PRINCIPLES/Interface_Segregation_Principle/PlaceOrder.java
@@ -0,0 +1,15 @@
+package com.ISP;
+
+
+//public interface OrderInterface 
+//{
+//    void placeOrder();
+//    void cancelOrder();
+//    void modifyOrder();
+//    void processPayment();
+//}
+
+public interface PlaceOrder
+{
+    void placeOrder();
+}
